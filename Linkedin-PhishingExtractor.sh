0c0f841785bb6d92608b457336ba2d08fabd98ae
norseh <hori@hesron.com.br>
2020-02-20 15:50:58 -0300

    Update Linkedin-PhishingExtractor.sh

--- Linkedin-PhishingExtractor.sh
+++ Linkedin-PhishingExtractor.sh
@@ -4,3 +4,4 @@
 # call this script with argument the .webarchive file from Linkedin Org's page -> People -> Wait to load all people -> "Command + s" -> Format: Web Archive
 
 cat $1 | strings | grep -i "org-people-profile-card__profile-title" | nl | awk -F "ember-view\">" '{print $3,"\""$5}' | awk -F "\"" '{print "\"https://www.linkedin.com" $4 "\",\"" $9 "\""}' | gsed 's/\s\s/ /g' | gsed 's/  / /g' | gsed 's/" /"/g'
+#
